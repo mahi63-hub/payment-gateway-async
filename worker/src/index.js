6820049c9b1abca233b7b5dfc12683547d8e850a
MAHI RALI <23MH1A4963@acoe.edu.in>
2026-01-14 11:16:00 +0530

    worker service skeleton

---
+++ worker/src/index.js
@@ -0,0 +1,9 @@
+require("dotenv").config();
+
+console.log("Worker service started");
+
+// later we will:
+// 1. connect to Redis
+// 2. process payment jobs
+// 3. process refund jobs
+// 4. deliver webhooks
